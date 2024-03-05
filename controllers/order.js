ad02adfca1253e48c242abc5209c6f34ec4904eb
jesseca.casanare <jesseca.casanare@zapier.com>
2024-03-05 17:37:53 +0800

    added notes in order.js

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,5 @@
 const Order = require("../models/Order");
-const {getCart} = require("./cart");
+const Cart = require("../models/Cart");
 
 
 
@@ -16,3 +16,29 @@ module.exports.allOrders = (req, res) => {
 module.exports.checkout = (req, res) => {
 
 }
+
+
+// module.exports.getCart = (req, res) => {
+
+// 	if (req.user.isAdmin) {
+// 		return res.status(403).send({ message: "Admins are forbidden to have a cart." });
+// 	}
+
+// 	Cart.findOne({
+// 		userId: req.user.id
+// 	})
+// 		.then(foundCart => {
+// 			if (!foundCart) {
+// 				return res.status(404).send({ message: "Cart not found." });
+// 			}
+// 			return res.status(200).send({
+// 				message: "User's cart found.",
+// 				userCart: foundCart
+// 			});
+// 		})
+// 		.catch(findErr => {
+// 			console.error("Error in finding cart: ", err);
+// 			return res.send(500).send({ error: "Failed to find cart." });
+// 		});
+
+// };
