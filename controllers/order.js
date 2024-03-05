f8716d954f84abe2d413bcad144f208d364334ca
jesseca.casanare <jesseca.casanare@zapier.com>
2024-03-05 19:57:01 +0800

    createOrder completed

--- controllers/order.js
+++ controllers/order.js
@@ -4,33 +4,48 @@ const Cart = require("../models/Cart");
 
 module.exports.createOrder = (req, res) => {
 
-    Cart.findOne({ userId: req.user.id })
-        .then(foundCart => {
-            if (!foundCart) {
-                return res.status(404).send({ message: "Cant checkout - Cart Not Found." });
+    Order.find({ userId: req.user.id, status: "Pending" })
+        .then(pendingOrders => {
+            if (pendingOrders.length > 0) {
+                return res.status(200).send({ pendingOrder })
             }
 
             else {
-                let newOrder = new Order({
-                    userId: req.user.id,
-                    orderItems: req.body.cartItems
-                })
+                Cart.findOne({ userId: req.user.id })
+                    .then(foundCart => {
+                        if (!foundCart) {
+                            return res.status(404).send({ message: "Cant checkout - Cart Not Found." });
+                        }
 
-                newOrder.save()
-                    .then(savedOrder => {
-                        return res.status(200).send({
-                            message: "Order successfully created",
-                            newOrder: savedOrder
-                        });
+                        else {
+                            let newOrder = new Order({
+                                userId: req.user.id,
+                                orderItems: req.body.cartItems
+                            })
+
+                            newOrder.save()
+                                .then(savedOrder => {
+                                    return res.status(200).send({
+                                        message: "Order successfully created",
+                                        newOrder: savedOrder
+                                    });
+                                })
+                                .catch(error => {
+                                    return res.status(500).send({ error: `Can't checkout - Error in saving order: ${error}` });
+                                });
+                        }
                     })
-                    .catch(error => {
-                        return res.status(500).send({ error: `Can't checkout - Error in saving order: ${error}` });
+                    .catch(err => {
+                        return res.send(500).send({ error: `Cant checkout - Error in finding cart: ${err}` });
                     });
+
             }
         })
+
         .catch(err => {
-            return res.send(500).send({ error: `Cant checkout - Error in finding cart: ${err}` });
+            return res.send(500).send({ error: `Error in retrieving Pending Orders: ${err}` });
         });
+
 }
 
 
--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        required: [true, 'Order must be associated with a User']
+        required: [true, "Order must be associated with a User"]
     },
     productsOrdered: [{
         productId: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-            required: [true, 'ProductId is required']
+            ref: "Product",
+            required: [true, "ProductId is required"]
         },
         quantity: {
             type: Number,
@@ -30,8 +30,8 @@ const orderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        default: 'Pending'
+        default: "Pending"
     }
 });
 
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
