19146520fa5c27655e161bc76b4c43a71e1a6614
rjstan03 <rjstan03@gmail.com>
2024-04-02 19:30:23 +0800

    update user controller

--- controllers/user.js
+++ controllers/user.js
@@ -44,78 +44,110 @@ module.exports.registerUser = (req,res) => {
 
 
 // LOGIN USER
-module.exports.loginUser = (req, res) => {
-    if (req.body.email.toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        )
-    ) {
-        User.findOne({ email: req.body.email })
-            .then(result => {
-                if (result == null) {
-                    return res.status(404).send({ error: "No Email Found" });
+module.exports.loginUser = (req,res) => {
+    // The "findOne" method returns the first record in the collection that matches the search criteria
+    // We use the "findOne" method instead of the "find" method which returns all records that match the search criteria
+    if(req.body.email.includes("@")){
+        return User.findOne({ email : req.body.email })
+        .then(result => {
+
+            // User does not exist
+            if(result == null){
+
+                // Send the message to the user
+                return res.status(404).send({ error: "No Email Found" });
+
+            // User exists
+            } else {
+
+                // Creates the variable "isPasswordCorrect" to return the result of comparing the login form password and the database password
+                // The "compareSync" method is used to compare a non encrypted password from the login form to the encrypted password retrieved from the database and returns "true" or "false" value depending on the result
+                // A good coding practice for boolean variable/constants is to use the word "is" or "are" at the beginning in the form of is+Noun
+                    //example. isSingle, isDone, isAdmin, areDone, etc..
+                const isPasswordCorrect = bcrypt.compareSync(req.body.password, result.password);
+
+                // If the passwords match/result of the above code is true
+                if (isPasswordCorrect) {
+
+                    // Generate an access token
+                    // Uses the "createAccessToken" method defined in the "auth.js" file
+                    // Returning an object back to the client application is common practice to ensure information is properly labeled and real world examples normally return more complex information represented by objects
+                    return res.status(200).send({ access : auth.createAccessToken(result)})
+
+                // Passwords do not match
+                } else {
+
+                    return res.status(401).send({ message: "Email and password do not match" });
+
                 }
 
-                else {
-                    const isPasswordCorrect = bcrypt.compareSync(req.body.password, result.password);
+            }
 
-                    if (isPasswordCorrect) {
-                        return res.status(200).send({ access: auth.createAccessToken(result) })
-
-                    }
-
-                    else {
-                        return res.status(401).send({ message: "Email and password do not match" });
-                    }
-                }
-            })
-            .catch(err => {
-                return res.status(500).send({ error: `User Not Found: ${err}` });
-            })
+        })
+        .catch(err => err);
+    } else {
+        return res.status(400).send({ error: "Invalid Email" })
     }
-
-    else {
-        return res.status(400).send({ error: "Email format invalid" });
-    }
-}
+};
 
 // RESET PASSWORD
 module.exports.resetPassword = async (req, res) => {
-    try {
-        const { newPassword } = req.body;
-        const { id } = req.user;
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
 
+    try {
+
+        // Add a console.log() to check if you can pass data properly from postman
+        // console.log(req.body);
+
+        // Add a console.log() to show req.user, our decoded token, does not contain userId property but instead id
+        // console.log(req.user);
+
+        const { newPassword } = req.body;
+
+        // update userId to id because our version of req.user does not have userId property but id property instead.
+        const { id } = req.user; // Extracting user ID from the authorization header
+
+        // Hashing the new password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        
+        // Update userId update to id
+        // Updating the user's password in the database
         await User.findByIdAndUpdate(id, { password: hashedPassword });
 
-        res.status(200).send({ message: 'Reset Successful' });
+        // Sending a success response
+        res.status(200).json({ message: 'Password reset successfully' });
+
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+
     }
 
-    catch (err) {
-        res.status(500).send({ message: `Reset Error: ${err}` });
-    }
 };
 
 
 // GET OWN USER DETAILS
 module.exports.getProfile = (req, res) => {
-    const userId = req.user.id;
 
-    User.findById(userId)
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({ error: 'User Not Found' });
-            }
+    // The "return" keyword ensures the end of the getProfile method.
+    // Since getProfile is now used as a middleware it should have access to "req.user" if the "verify" method is used before it.
+    // Order of middlewares is important. This is because the "getProfile" method is the "next" function to the "verify" method, it receives the updated request with the user id from it.
+    return User.findById(req.user.id)
+    .then(user => {
+        if (!user) {
+            return res.status(404).send({ error: 'User not found' });
+        }
 
-            else {
-                return res.status(200).send({ user });
-            }
+        // Exclude sensitive information like password
+        user.password = undefined;
 
-        })
+        return res.status(200).send({ user });
+    })
+    .catch(err => {
+        console.error("Error in fetching user profile", err)
+        return res.status(500).send({ error: 'Failed to fetch user profile' })
+    });
 
-        .catch(err => {
-            return res.status(500).send({ error: `GET Profile Failed: ${err}` })
-        })
 };
 
 
