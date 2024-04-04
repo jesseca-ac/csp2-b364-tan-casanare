5c18db83be295dc2b7cd8dc84b04c4e927ba4679
rjstan03 <rjstan03@gmail.com>
2024-04-04 12:31:57 +0800

    updated auth

--- auth.js
+++ auth.js
@@ -1,45 +1,72 @@
-require('dotenv').config()
+//require('dotenv').config()
 const jwt = require("jsonwebtoken");
-const secret = "ECommerceAPI";
+const secret = "CourseBookingAPI";
 
 // Creates Access Token
 module.exports.createAccessToken = (user) => {
+    // The data will be received from the registration form
+    // When the user logs in, a token will be created with user's information
     const data = {
-        id: user._id,
-        email: user.email,
-        isAdmin: user.isAdmin
-    }
+        id : user._id,
+        email : user.email,
+        isAdmin : user.isAdmin
+    };
 
+    // Generate a JSON web token using the jwt's sign method
+    // Generates the token using the form data and the secret code with no additional options provided
     return jwt.sign(data, secret, {});
-}
+    
+};
 
 // Verify User
 module.exports.verify = (req, res, next) => {
-    let token = req.headers.authorization
+    console.log(req.headers.authorization);
 
-    if (typeof token === "undefined") {
-        return res.status(401).send({
-            auth: "Failed",
-            message: "Verification Failed - No Token"
-        })
+    // "req.headers.authorization" contains sensitive data and especially our token
+    let token = req.headers.authorization;
 
+    // This if statement will first check if a token variable contains "undefined" or a proper jwt.  we will check token's data type with "typeof", if it is "undefined" we will send a message to the client. Else if it is not, then we return the token.
+    if(typeof token === "undefined"){
+        return res.send({ auth: "Failed. No Token" });
     } else {
-        token = token.slice(7, token.length)
+        console.log(token);     
+        token = token.slice(7, token.length);
+        console.log(token);
 
-        jwt.verify(token, secret, function (err, decodedToken) {
-            if (err) {
-                return res.status(401).send({
+        //[SECTION] Token decryption
+        /*
+        Analogy
+            Open the gift and get the content
+        - Validate the token using the "verify" method decrypting the token using the secret code.
+        - token - the jwt token passed from the request headers.
+        - secret - the secret word from earlier which validates our token
+        - function(err,decodedToken) - err contains the error in verification, decodedToken contains the decoded data within the token after verification
+        */
+        // "err" is a built-in variable of express to handle errors
+        jwt.verify(token, secret, function(err, decodedToken){
+            
+            //If there was an error in verification, an erratic token, a wrong secret within the token, we will send a message to the client.
+            if(err){
+                return res.send({
                     auth: "Failed",
                     message: err.message
-                })
+                });
 
             } else {
+
+                // Contains the data from our token
+                console.log("result from verify method:")
+                console.log(decodedToken);
+                
+                // Else, if our token is verified to be correct, then we will update the request and add the user's decoded details.
                 req.user = decodedToken;
+
+                // next() is an expressJS function which allows us to move to the next function in the route. It also passes details of the request and response to the next function/middleware.
                 next();
             }
         })
     }
-}
+};
 
 // Verify if User is Admin
 module.exports.verifyAdmin = (req, res, next) => {
