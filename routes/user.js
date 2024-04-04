787dc5d02f41634927d75033dd7e365df750cb85
rjstan03 <rjstan03@gmail.com>
2024-04-04 11:59:54 +0800

    updated user controller

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,12 @@ const router = express.Router();
 // User Access routes
 router.post("/", userController.registerUser);
 router.post("/login", userController.loginUser);
-router.get("/logout", userController.logoutUser);
-router.put('/reset-password', verify, userController.resetPassword);
-router.put('/profile', verify, userController.updateProfile);
+//router.get("/logout", userController.logoutUser);
+router.put("/reset-password", verify, userController.resetPassword);
+router.put("/profile", verify, userController.updateProfile);
 router.get("/details", verify, userController.getProfile);
 
 // Admin Access routes
-router.patch('/:userId/set-as-admin', verify, verifyAdmin, userController.setAdmin);
+//router.patch("/:userId/set-as-admin", verify, verifyAdmin, userController.setAdmin);
 
 module.exports = router;
